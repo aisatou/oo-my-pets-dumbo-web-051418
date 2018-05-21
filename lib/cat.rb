9682a5d675f1bfebaba410754eaafa404c75bc9a
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-05-21 20:57:31 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,7 +3,7 @@ class Cat
   attr_reader :name
   attr_writer :mood
   
-  def initialize(cat, name, mood)
+  def initialize(cat, name, mood = "nervous")
     @cat = cat
     @name = name
     @mood = "nervous"
